3b1aac4b0c499f304f8ebb40cadfc4e2ec833f72
raisaakilah <raisaakilah10@gmail.com>
2022-06-04 20:41:17 +0700

    raisa cantik

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Coding itu tidak sulit yah!</title>
+	<title>Coding itu seru yahhh!</title>
 
 	<style type="text/css">
 
